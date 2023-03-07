0f5124041c0ebdd57c8df8f13b7811f4523b2cf2
Darlene Soon <125565155+ddarlenee@users.noreply.github.com>
2023-03-07 04:29:54 +0000

    updated web app with html, css, flask jinja and text files

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
-    return 'Hello from Flask!'
+    with open('intro.txt', 'r') as file:
+      message = file.read()
+    data = None 
+    numbers = [1,2,3,4,5]
+    if request.method == "POST":
+      data = request.form['whatever']
+      with open('secret.txt', 'a') as file:
+        file.write(data + "\n")
+    return render_template('index.html', whatever = data, nums = numbers, intro = message)
 
 
 app.run(host='0.0.0.0', port=81)
